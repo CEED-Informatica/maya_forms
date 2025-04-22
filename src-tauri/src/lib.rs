1d75c824a4e780619279d0967a28f5428daee20c
Alfredo Oltra <aoltra@gmail.com>
2025-04-23 00:40:26 +0200

    build: muestra las devtool en el webview de la app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,12 +3,21 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 } */
+use tauri::Manager;
 
 mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+        #[cfg(debug_assertions)] // only include this code on debug builds
+        {
+            let window = app.get_webview_window("main").unwrap();
+            window.open_devtools();
+        }
+        Ok(())
+    })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![commands::greet])
