78efbaed151882521da35ef8e5b4056627913dca
Alfredo Oltra <aoltra@gmail.com>
2025-04-27 21:28:05 +0200

    feat: tooltip para dar información sobre la versión de los datos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
-import { Sun, Moon, User } from 'lucide-react';
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 import { useState, useEffect } from 'react'
 
@@ -22,13 +26,22 @@ export default function Footer() {
 
   return (
     <footer className='fixed bottom-0 px-2 w-full h-6 bg-gray-900 flex items-center justify-between text-gray-200 text-xs border-t border-gray-700 z-50'>
-      <div>
-        hola
-      </div>
-      <div className="text-right">
-        maya_forms <span className="pl-2">{appVersion}</span><span className="px-2">|</span> 
-        <span>{infoVersion}</span>
-      </div>
+      <TooltipProvider>
+        <div>
+          hola
+        </div>
+        <div className="text-right">
+          maya_forms <span className="pl-2">{appVersion}</span><span className="px-2">|</span>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>{infoVersion}</span>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Versión de los datos. Pulsa para actualizar</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      </TooltipProvider>
     </footer>
   );
 };
