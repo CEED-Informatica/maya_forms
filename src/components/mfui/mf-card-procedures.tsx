878c2a78745c983af34bc1b0b677ed82bfffb650
Alfredo Oltra <aoltra@gmail.com>
2025-05-20 09:13:34 +0200

    feat: se añade información sobre el estado de los trámites

--- src/components/mfui/mf-card-procedures.tsx
+++ src/components/mfui/mf-card-procedures.tsx
@@ -8,14 +8,47 @@ import {
 } from "@/components/ui/card"
 
 // iconos
-import { ArrowRight, FileText } from "lucide-react"
+import { ArrowRight, FileText, Lock, Unlock  } from "lucide-react"
 import { icons } from "@/components/mfui/icons"
 
 // React Router DOM
 import { Link } from "react-router-dom"
 
+import { isBefore, isAfter, differenceInDays, parseISO } from "date-fns"
+
+const DAYS_WARNING = 7
+
 export default function MFCardProcedures({data, color} : any) {
+  const now = new Date()
   const numProcedures = data.info?.subtypes?.length ?? 0
+
+  // cálculo del número de trámites por situacion
+  let countOpen = 0
+  let countClosingSoon = 0
+  let countClosed = 0
+
+  data.info.subtypes.forEach((subtype: any) => {
+    const key = Object.keys(subtype)[0]
+    const item = subtype[key]
+    const init = parseISO(item.init_date)
+    const end = parseISO(item.end_date)
+  
+    if (isBefore(now, init)) {
+      countClosed++
+      return 
+    }
+
+    if (isAfter(now, end)) {
+      countClosed++
+    } else {
+      const daysLeft = differenceInDays(end, now)
+      if (daysLeft <= DAYS_WARNING) {
+        countClosingSoon++
+      } else {
+        countOpen++
+      }
+    }
+  })
 
   return (
     <Card className="mx-4 my-4 cursor-pointer w-64 py-4 px-2 bg-card rounded-2xl shadow-md hover:bg-muted transition-colors duration-200 border border-border" 
@@ -29,7 +62,7 @@ export default function MFCardProcedures({data, color} : any) {
           </div>
         </div>
       </CardHeader>
-      <CardContent className="flex justify-start">
+      <CardContent className="flex flex-col items-start">
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger className="flex items-center space-x-2">
@@ -41,6 +74,46 @@ export default function MFCardProcedures({data, color} : any) {
             </TooltipContent>
           </Tooltip>
         </TooltipProvider>
+        <div className="flex items-center space-x-4 mt-5">
+          {/* Abiertos */}
+          {countOpen > 0 && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="flex items-center space-x-2">
+                  <Unlock className="text-green-600 w-5 h-5 animate-bounce" />
+                  <span className="text-sm text-green-700">{countOpen}</span>
+                </TooltipTrigger>
+                <TooltipContent>{countOpen} plazo abierto(s)</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+
+          {/* Próximos a cerrar */}
+          {countClosingSoon > 0 && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger  className="flex items-center space-x-2">
+                  <Unlock className="text-yellow-600 w-5 h-5  animate-pulse" />
+                  <span className="text-sm text-yellow-700">{countClosingSoon}</span>
+                </TooltipTrigger>
+                <TooltipContent>{countClosingSoon} plazo a punto de cerrar</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+
+          {/* Cerrados */}
+          {countClosed > 0 && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="flex items-center space-x-2">
+                  <Lock className="text-red-600 w-5 h-5" />
+                  <span className="text-sm text-red-700">{countClosed}</span>
+                </TooltipTrigger>
+                <TooltipContent>{countClosed} plazo cerrado(s)</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+        </div>
       </CardContent>
       <CardFooter>
         <Button asChild className="w-full" style={{ backgroundColor: color, borderColor: data.color }}>
