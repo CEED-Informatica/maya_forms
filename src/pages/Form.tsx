439bfe3272665c8f5e0a6dad2dcce527a5a6e5fc
Alfredo Oltra <aoltra@gmail.com>
2025-05-29 20:01:40 +0200

    feat: selección de ciclo formativo

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -4,7 +4,7 @@ import DynamicForm from "@/components/dynamic-form";
 export default function Form() {
 
   return  (
-    <div>
+    <div className="p-6">
       <DynamicForm formId="CF_studies_validation"/>
     </div>
   );
